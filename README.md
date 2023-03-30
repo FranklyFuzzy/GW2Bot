1d95bd25624af4ccf53949dd9ac9bf2d395f1603
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-03-29 23:08:42 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,20 @@
+# gw2api
+
+https://wiki.guildwars2.com/wiki/Template:Daily_Fractal_Schedule
+
+https://api.guildwars2.com/v2/achievements/daily
+
+https://api.guildwars2.com/v2/achievements?ids=1981
+
+working script code
+https://replit.com/ ### /IndigoLavishWorker
+
+#daily call
+https://replit.com/ *** /NeatNegativeHashmaps#main.py
+
+I wanted to know when certain dailies were available.
+To do this I leveraged the GW2 API found here.
+In order to best accomplish this I set out to discover what ids coincided with the daily activities I was interested in.
+I used a python script to check the dailies via the api and save the output with the name and associated ids. That save file also includes the date because I was curious if there was a pattern or loop for when acheivments are available or if it was random (more to come later).
+After getting a list of ids of interest, I set out to create a twitter bot that would use python to check the dailies and if the ids match with those of interest, tweets those dailies out so I can see the fun ones without logging in!
+at first I hosted the code locally. I then transitioned to using aws lambda to run the daily check and tweeting.
---
+++ ver2.py
@@ -0,0 +1,49 @@
+import requests
+print("Todays fun activities include ")
+url = "https://api.guildwars2.com/v2/achievements/daily"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    found = False
+    for daily in data["fractals"]:
+        x = [3973,2929,11111,2966]
+        for id in x:
+            if daily["id"] == id:
+                url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
+                response = requests.get(url)
+                fractals_data = response.json()
+                print(fractals_data[0]["name"], fractals_data[0]["id"])
+                found = True
+                break
+        if not found:
+            continue
+    found = False
+    for daily in data["wvw"]:
+        x = [1848]
+        for id in x:
+            if daily["id"] == id:
+                url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
+                response = requests.get(url)
+                wvw_data = response.json()
+                print(wvw_data[0]["name"], wvw_data[0]["id"])
+                found = True
+                break
+        if not found:
+            continue
+    found = False
+    for daily in data["pve"]:
+        x = [1981]
+        for id in x:
+            if daily["id"] == id:
+                url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
+                response = requests.get(url)
+                pve_data = response.json()
+                print(pve_data[0]["name"], pve_data[0]["id"])
+                found = True
+                break
+        if not found:
+            continue
+else:
+    print("Error: Could not retrieve data from the API.")
