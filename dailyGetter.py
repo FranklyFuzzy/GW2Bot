664487efd07e69960169906b09b6a2ae53192459
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-03-31 22:54:53 -0400

    Update dailyGetter.py
    Move print from the loop location to the top.

--- dailyGetter.py
+++ dailyGetter.py
@@ -1,5 +1,5 @@
 import requests
-
+print("Todays fun activities include ")
 url = "https://api.guildwars2.com/v2/achievements/daily"
 
 response = requests.get(url)
@@ -14,7 +14,7 @@ if response.status_code == 200:
                 url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
                 response = requests.get(url)
                 fractals_data = response.json()
-                print("Todays fun activities include ", fractals_data[0]["name"], fractals_data[0]["id"])
+                print(fractals_data[0]["name"], fractals_data[0]["id"])
                 found = True
                 break
         if not found:
@@ -27,12 +27,26 @@ if response.status_code == 200:
                 url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
                 response = requests.get(url)
                 wvw_data = response.json()
-                print("Todays fun activities include ", wvw_data[0]["name"], wvw_data[0]["id"])
+                print(wvw_data[0]["name"], wvw_data[0]["id"])
                 found = True
                 break
         if not found:
             continue
     found = False
+    for daily in data["pve"]:
+        x = [1981]
+        for id in x:
+            if daily["id"] == id:
+                url = "https://api.guildwars2.com/v2/achievements?ids=" + str(id)
+                response = requests.get(url)
+                pve_data = response.json()
+                print(pve_data[0]["name"], pve_data[0]["id"])
+                found = True
+                break
+        if not found:
+            continue
+else:
+    print("Error: Could not retrieve data from the API.")
     for daily in data["pve"]:
         x = [1981]
         for id in x:
