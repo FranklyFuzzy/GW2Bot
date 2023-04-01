ce7f308347a03946495da7ec699b3f0814a67357
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-03-31 23:00:21 -0400

    Update dailyGetter.py
    added datetime and printing output to file

--- dailyGetter.py
+++ dailyGetter.py
@@ -1,4 +1,11 @@
 import requests
+import datetime
+
+# Get the current date
+now = datetime.datetime.now()
+
+# Format the date as YYYY-MM-DD
+date_string = now.strftime('%Y-%m-%d')
 
 url = "https://api.guildwars2.com/v2/achievements/daily"
 response = requests.get(url)
@@ -9,8 +16,10 @@ for key in data.keys():
     if key == "pve" or key == "pvp" or key == "wvw" or key == "fractals" or key == "special":
         for i in range(len(data[key])):
             ids.append(data[key][i]["id"])
-for x in ids:
-    url = "https://api.guildwars2.com/v2/achievements?ids=" + str(x)
-    response = requests.get(url)
-    data = response.json()
-    print(data[0]["name"], data[0]["id"])
+with open(f'output_{date_string}.txt', 'w') as f:
+    for x in ids:
+        url = "https://api.guildwars2.com/v2/achievements?ids=" + str(x)
+        response = requests.get(url)
+        data = response.json()
+        print(data[0]["name"], data[0]["id"])
+        print(data[0]["name"], data[0]["id"], file =f)
