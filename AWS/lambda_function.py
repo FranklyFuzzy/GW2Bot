c43ccc0718a83289b0bd2df25c6b62f5183de20f
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-03-31 23:07:21 -0400

    Update lambda_function.py
    removed hard coded creds for AWS SSM

--- AWS/lambda_function.py
+++ AWS/lambda_function.py
@@ -1,16 +1,24 @@
 import tweepy
+import boto3
 
-consumer_key = "DontHardCodeCreds"
-consumer_secret = "DontHardCodeCreds"
-access_token = "DontHardCodeCreds"
-access_token_secret = "DontHardCodeCreds"
+# Create an SSM client object
+ssm = boto3.client('ssm')
 
-client = tweepy.Client(
+def lambda_handler(event, context):
+    # Retrieve the API credentials from the Parameter Store
+    consumer_key = ssm.get_parameter(Name='/twitter-api-keys/consumer-key', WithDecryption=True)['Parameter']['Value']
+    consumer_secret = ssm.get_parameter(Name='/twitter-api-keys/consumer-secret', WithDecryption=True)['Parameter']['Value']
+    access_token = ssm.get_parameter(Name='/twitter-api-keys/access-token', WithDecryption=True)['Parameter']['Value']
+    access_token_secret = ssm.get_parameter(Name='/twitter-api-keys/access-secret', WithDecryption=True)['Parameter']['Value']
+
+    # Authenticate with Twitter API
+    client = tweepy.Client(
     consumer_key=consumer_key, consumer_secret=consumer_secret,
     access_token=access_token, access_token_secret=access_token_secret
-)
-
-response = client.create_tweet(
-    text="This Tweet was Tweeted using Tweepy and Twitter API v2!"
-)
-print(f"https://twitter.com/user/status/{response.data['id']}")
+    )
+    
+    # Post a tweet
+    response = client.create_tweet(
+    text="Hello from AWS"
+    )
+    print(f"https://twitter.com/user/status/{response.data['id']}")
