37b078d81d5cdc85b022367450aa9c55d263b82b
FranklyFuzzy <30601526+FranklyFuzzy@users.noreply.github.com>
2023-03-31 22:59:23 -0400

    Update dailyGetter.py
    Remove part that looks for specific ids in response.json so I can grab all the dailies and print them as name & id.

--- dailyGetter.py
+++ dailyGetter.py
@@ -9,16 +9,8 @@ for key in data.keys():
     if key == "pve" or key == "pvp" or key == "wvw" or key == "fractals" or key == "special":
         for i in range(len(data[key])):
             ids.append(data[key][i]["id"])
-
-ids_to_check = [2908, 3201, 2989, 1930]
-
-if any(x in ids_to_check for x in ids):
-    print("Today's fun activities include :")
-    for x in ids_to_check:
-        if x in ids:
-            url = "https://api.guildwars2.com/v2/achievements?ids=" + str(x)
-            response = requests.get(url)
-            data = response.json()
-            print(data[0]["name"], data[0]["id"])
-else:
-  print("no fun today")
+for x in ids:
+    url = "https://api.guildwars2.com/v2/achievements?ids=" + str(x)
+    response = requests.get(url)
+    data = response.json()
+    print(data[0]["name"], data[0]["id"])
